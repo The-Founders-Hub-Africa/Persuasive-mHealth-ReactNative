2f68a0a64c63af032fc9216cc4a0a5bb13039ef9
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-06-20 15:35:28 +0100

    tabs and headers

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,13 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Tabs } from 'expo-router';
-import { Ionicons } from "@expo/vector-icons";
+import { Tabs, useRouter } from 'expo-router';
+import { Feather, Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from 'react-native';
+import theme from '@/styles/theme';
 
 
 export default function TabLayout() {
+
+  const navigation = useRouter();
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
       <Tabs.Screen name='home/index'
@@ -15,16 +19,33 @@ export default function TabLayout() {
       />
 
        <Tabs.Screen name='patients/index'
-      options={{
+          options={{
           title: 'Patients',
           tabBarIcon: ({ color }) => <Ionicons size={28} name="people-outline" color={color} />,
-
+          headerRight: () => (
+            <TouchableOpacity onPress={()=>navigation.navigate("../newPatient")}>
+              <Feather
+                name="plus"
+                size={24}
+                color={theme.colors["neutral-700"]}
+              />
+            </TouchableOpacity>
+          ),
         }}
       />
 
        <Tabs.Screen name='appointments/index'
       options={{
           title: 'Appointments',
+          headerRight: () => (
+            <TouchableOpacity onPress={()=>navigation.navigate("../newAppointments")}>
+              <Feather
+                name="plus"
+                size={24}
+                color={theme.colors["neutral-700"]}
+              />
+            </TouchableOpacity>
+          ),
           tabBarIcon: ({ color }) => <Ionicons size={28} name="calendar-outline" color={color} />,
 
         }}
