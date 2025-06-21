9b1af3604df9231272b7a666473a2d2bfa8851be
EMEKA ONWUEPE <pascalemy2010@gmail.com>
2025-06-21 14:47:58 +0100

    restructuring

--- app/(dashboard)/(home)/_layout.tsx
+++ app/(dashboard)/(home)/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from 'expo-router';
 
 export default function HomeLayout() {
-    return  <Stack screenOptions={{ headerShown: false }} />
-    
-}
+
+    return  (
+        <Stack screenOptions={{ headerShown: false }} >
+        <Stack.Screen name="analytics"  options={{ title: 'Analytics' }}/>
+        <Stack.Screen name="notifications" options={{ title: 'Notifications' }} />
+        <Stack.Screen name="search" options={{ title: 'Search' }} />
+        <Stack.Screen name="viewProfile" options={{ title: 'View Profile' }} />
+        </Stack>
+    )}
